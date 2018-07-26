867e5a61e8c6abbf0881dc5588600085d89740c3
ricardo2018-git <rp_mira@hotmail.com>
2018-07-25 21:22:21 -0300

    class crud função select

--- require/class/CRUD.class.php
+++ require/class/CRUD.class.php
@@ -1,20 +1,32 @@
 <?php
 	
-	// -- Herda da class ConDB.class.php --
-	class CRUD extends ConDB{
-		private $query;
-		
-		private function prepExec($prep, $exec){
-			$this -> query = $this -> getConn() -> prepare($prep);
-			$this -> query -> execute($exec);
+	# -- Herda da class ConDB.class.php --
+		class CRUD extends ConDB{
+
+			private $query;
+			
+			private function prepExec($prep, $exec){
+				$this -> query = $this -> getConn() -> prepare($prep);
+				$this -> query -> execute($exec);
+			}
+
+			# -- Insere dados no banco --
+				public function insert($table, $prep, $exec){
+
+					$this -> prepExec('INSERT INTO '.$table.' SET '.$prep.'', $exec);
+
+					# -- Retorna o ultimo id gerado no banco --
+					return $this -> getConn() -> lastInsertId();
+				}
+			# -- END Insere dados no banco --
+
+
+			# -- Faz uma busca no banco --
+				public function select($fields, $table, $prep, $exec){
+					$this -> prepExec('SELECT '.$fields.' FROM '.$table.' '.$prep.' ', $exec);
+					return $this -> query;
+				}
+			# -- END Faz uma busca no banco --
 		}
-
-		public function insert($table, $prep, $exec){
-
-			$this -> prepExec('INSERT INTO '.$table.' SET '.$prep.'', $exec);
-
-			// -- Retorna o ultimo id gerado no banco --
-			return $this -> getConn() -> lastInsertId();
-		}
-	}
+	# -- END Herda da class ConDB.class.php --
 ?>
--- require/class/ConDB.class.php
+++ require/class/ConDB.class.php
@@ -1,28 +1,44 @@
 <?php
 
-	// -- Carrega class´s automaticamente --
+	# -- Carrega class´s automaticamente --
 	function __autoload($class){
 		require_once"{$class}.class.php";
 	}
 
-	// -- Conexao com o banco --
-	abstract class ConDB{
+	# -- Conexao com o banco --
+		abstract class ConDB{
 
-		private static $cnx;
+			private static $cnx;
 
-		private function setConn(){
-			return
-			is_null(self::$cnx) ?
-					self::$cnx = new PDO("mysql:host=localhost;dbname=desenvolvendophp", "root", "") :
-					self::$cnx;
+			private function setConn(){
+				return
+				is_null(self::$cnx) ?
+						self::$cnx = new PDO("mysql:host=localhost;dbname=desenvolvendophp", "root", "") :
+						self::$cnx;
+			}
+
+			public function getConn(){
+				return $this -> setConn();
+			}
 		}
+	# -- END Conexao com o banco --
 
-		public function getConn(){
-			return $this -> setConn();
-		}
-	}
-
-	// -- Insere um registro no banco --
+	# -- Instancia um objeto --
 	$crud = new CRUD;
-	$crud -> insert('user', 'user=?, email=?, cidade=?', array('nomeDoUsuario', 'teste@hotmail.com', 'cidadeDeDeus'));
+
+/*
+	# -- chama class crud e faz um insert --
+		$crud -> insert('user', 'user=?, email=?, cidade=?', array('nomeDoUsuario', 'teste@hotmail.com', 'cidadeDeDeus'));
+	# -- END chama class crud e faz um insert --
+
+	# -- Faz uma busca no banco --
+		$sel = $crud -> select('*', 'user', 'WHERE idUser = ?', array(2));
+		foreach($sel as $reg){
+			$_SESSION['user'] = $reg;
+		}
+	# -- END Faz uma busca no banco --
+
+
+	var_dump($_SESSION['user']['email']);
+*/
 ?>
