95f975533f831160a87bd612e7e74e1af2ee8feb
ricardo2018-git <rp_mira@hotmail.com>
2018-07-31 21:22:53 -0300

    tela de login html e jquery min

--- login.php
+++ login.php
@@ -3,6 +3,9 @@
 	<head>
 		<meta charset="UTF-8">	<!-- p/ reconhecer os caracteres especiais -->
 		<title>Projeto Loja Virtual !!!</title>
+		<link type="text/css" rel="stylesheet" href="require/css/login.css" />
+		<script type="text/javascript" src="require/js/jquery-3.3.1.min.js"></script>
+		<script type="text/javascript" src="require/js/login.js"></script>
 	</head>
 
 	<body>
@@ -15,7 +18,13 @@
 
 			<!-- Conteudo -->
 				<main>
-					
+					<form>
+						<p>Login no Desenvolvimento php</p>
+						<div>E-mail:<input type="text" id="eLogin" /></div>
+						<div>Senha:<input type="text" id="sLogin" /></div>
+						<div><button>Entrar</button></div>
+						<span></span>
+					</form>
 				</main>
 			<!-- FIM Conteudo -->
 
